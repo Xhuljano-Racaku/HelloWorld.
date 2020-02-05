01b137b22e96e58f0883ce8e36c42f24c0329af8
Xhuljano <racakuxhuljano@yahoo.com>
2020-02-05 09:59:19 -0500

    Added a button and display a message.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,8 +3,15 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+
+        Toast.makeText(this, "You have clicked the button", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
